0a92ce07e80de46ed4888861ed16441e570bc664
JoelPescador <joelpescadorserrano@gmail.com>
2024-09-11 08:15:02 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -11,3 +11,13 @@ const observador = new IntersectionObserver((entradas) => {
 
 const elementosOcultos = document.querySelectorAll(".oculto");
 elementosOcultos.forEach((elemento) => observador.observe(elemento));
+function svgParpadea() {
+  const contacto = document.getElementById("contacto");
+  const svgs = document.getElementsByClassName("animar");
+
+  contacto.addEventListener("click", function () {
+    for (let i = 0; i < svgs.length; i++) {
+      svgs[i].classList.add("animacion");
+    }
+  });
+}
